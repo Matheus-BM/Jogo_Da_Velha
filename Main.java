4a70defa7a44136dbe81e0061a1ae10d2dd1e109
MatheusBM <71776671+Matheus-BM@users.noreply.github.com>
2021-05-09 16:47:39 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+package br.ucsal4.uni2.proj;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	public static void imprimir(String txt) {
+		System.out.print(txt);
+	}
+	public static String scan() {
+		Scanner scan = new Scanner(System.in);
+		String res = scan.nextLine();
+		
+		return res;
+	}
+
+}
